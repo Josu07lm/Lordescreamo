4c6a4ced86b598208792f2b471510d977e468fa6
Josu07lm <81869195+Josu07lm@users.noreply.github.com>
2021-04-26 19:27:34 -0500

    Add files via upload

---
+++ numowner.js
@@ -0,0 +1,21 @@
+const ownerNumber = ["593992390339@s.whatsapp.net","+593992390339@s.whatsapp.net","6287894610286@s.whatsapp.net"]
+
+
+const vcard = 'BEGIN:VCARD\n'
+            + 'VERSION:3.0\n'
+            + 'FN:LORDE SCREAMO\n'
+            + 'ORG:Owner CAFE 𝙱𝙾𝚃;\n'
+            + 'TEL;type=CELL;type=VOICE;waid=593992390339:+593 99 239 0339\n'
+            + 'END:VCARD'
+
+            
+const vcard1 = 'BEGIN:VCARD\n'
+            + 'VERSION:3.0\n'
+            + 'FN:Cortes Screamo\n'
+            + 'ORG:Co Owner Lorde 𝙱𝙾𝚃;\n'
+            + 'TEL;type=CELL;type=VOICE;waid=593992390339:+593 99 239 0339\n'
+            + 'END:VCARD'
+
+exports.ownerNumber = ownerNumber
+exports.vcard = vcard
+exports.vcard1 = vcard1
